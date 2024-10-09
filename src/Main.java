d051d7a64a8a1bfb7110033cb2cfcf9b94f31fbb
schedowka <marina_molodkina@proton.me>
2024-10-09 17:51:06 +0300

    add some code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+//hgjhgjjf
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println("введи три чсила:" +a +b +c);
+        double d = (b*b) - 4*a*c;
+        if (d>0) {
+            System.out.println("вещественных корней 2: " + d);
+        } else if (d==0) {
+            System.out.println("вещественных корней 1: " + d);
+        } else {
+            System.out.println("вещественных корней нет");
+        }
+
+
+        }
 }
